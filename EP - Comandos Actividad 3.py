66be0c542f926c0660ed0cea52f256471240c2b9
Fede Rovner <68130084+federovner@users.noreply.github.com>
2020-08-25 17:51:10 -0300

    Update EP - Comandos Actividad 3.py

--- EP - Comandos Actividad 3.py
+++ EP - Comandos Actividad 3.py
@@ -1,27 +1,27 @@
-scrapy shell
-fetch("https://doi.org/10.1371/journal.pone.0005738")
+scrapy shell \n
+fetch("https://doi.org/10.1371/journal.pone.0005738") \n
 
-import html2text
-converter=html2text.HTML2Text()
-converter.ignore_links=True
-converter.ignore_images=True
-converter.ignore_tables=True
-texto=converter.handle(response.css('*').get())
-texto=texto.encode('ascii', 'ignore')
-f1=open("paper1.txt",'wb')
-f1.write(texto)
-f1.close()
+import html2text \n
+converter=html2text.HTML2Text() \n
+converter.ignore_links=True \n
+converter.ignore_images=True \n
+converter.ignore_tables=True \n
+texto=converter.handle(response.css('*').get()) \n
+texto=texto.encode('ascii', 'ignore') \n
+f1=open("paper1.txt",'wb') \n
+f1.write(texto) \n
+f1.close() \n
 
-fetch("http://www.gutenberg.org/files/244/244-h/244-h.htm")
-converter=html2text.HTML2Text()
-converter.ignore_links=True
-converter.ignore_images=True
-converter.ignore_tables=True
-texto=converter.handle(response.css('*').get())
-texto=texto.encode('ascii', 'ignore')
-f2=open("AStudyinScarlet.txt",'wb')
-f2.write(texto)
-f2.close()
+fetch("http://www.gutenberg.org/files/244/244-h/244-h.htm") \n
+converter=html2text.HTML2Text() \n
+converter.ignore_links=True \n
+converter.ignore_images=True \n
+converter.ignore_tables=True \n
+texto=converter.handle(response.css('*').get()) \n
+texto=texto.encode('ascii', 'ignore') \n
+f2=open("AStudyinScarlet.txt",'wb') \n
+f2.write(texto) \n
+f2.close() \n
 
 fetch("http://www.gutenberg.org/files/244/244-h/244-h.htm")
 converter=html2text.HTML2Text()
